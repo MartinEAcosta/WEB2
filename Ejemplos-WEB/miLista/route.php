81f015fbed8851de09a5b08e1c4465ffc8aa77cb
Martin Ezequiel Acosta <acostamartin1270@gmail.com>
2022-09-14 11:25:28 -0300

    Agrego Carpeta ejemplos y practico 2

---
+++ Ejemplos-WEB/miLista/route.php
@@ -0,0 +1,27 @@
+<?php
+
+    require_once 'about.php';
+
+    // leemos la accion que viene por parametro
+    $action = 'home'; // acción por defecto
+
+    if (!empty($_GET['action'])) { // si viene definida la reemplazamos
+        $action = $_GET['action'];
+    }
+
+
+    // parsea la accion Ej: dev/juan --> ['dev', juan]
+    $params = explode('/', $action);
+
+    // determina que camino seguir según la acción
+    switch ($params[0]) {
+        case 'home':
+            showHome();
+            break;
+        case 'about':
+            showAbout();
+            break;
+        default:
+            echo('404 Page not found');
+            break;
+    }
